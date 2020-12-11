2bf9ed543e22a454121efee19ef75325e843f7ce
Sarah Poulton <poultonse@gmail.com>
2020-12-10 21:51:52 -0600

    finished assignment

--- script.js
+++ script.js
@@ -12,29 +12,57 @@
 <img src="${}">
 */
 
+async function fetchTarget () {
+   try{   
+      let response = await fetch('https://handlers.education.launchcode.org/static/planets.json'); 
+      let json = await response.json();
+
+      let missionTarget = document.getElementById("missionTarget"); 
+      let index = Math.floor(Math.random()*json.length);  
+      missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+          <ol>
+            <li>Name: ${json[index].name}</li>
+            <li>Diameter: ${json[index].diameter}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
+           </ol>
+          <img src="${json[index].image}">`
+   } catch(err){ 
+         console.log(err); 
+   }
+};
+
 window.addEventListener("load", function(){
+   fetchTarget(); 
 
    let form = document.getElementById("launchForm"); 
+
    form.addEventListener("submit", function(event){
 
-      let pilotName = document.querySelector("input[name=pilotName]"); 
+
+      let pilotName = document.querySelector("input[name=pilotName]").value; 
       let copilotName = document.querySelector("input[name=copilotName").value; 
       let fuelLevel = document.querySelector("input[name=fuelLevel").value; 
       let cargoMass = document.querySelector("input[name=cargoMass").value; 
       let launchStatus = document.getElementById("launchStatus"); 
       fuelLevel = Number(fuelLevel); 
       cargoMass = Number(cargoMass); 
+      let nonletters = ['`', '~', '!', '@', "#", '$', '%', '^', '7', '*', "(", ')', '-', "_", '+', '=']; 
 
       if (pilotName.value === "" || copilotName === "" || fuelLevel === "" || cargoMass === ""){
          alert("All fields are required"); 
          event.preventDefault(); 
     };
-
+      // if (pilotName.includes(nonletters)){
+      //    alert("That is not an appropriate name for a pilot.")
+      //    event.preventDefault(); 
+      // }; 
       let updatedHtml = `
-      <div  id="faultyItems">
                 <ol>
-                    <li id="pilotStatus">${pilotName.value} Ready</li>
-                    <li id="copilotStatus">${copilotName} Ready</li>
+                    <li id="pilotStatus">Pilot ${pilotName} Ready</li>
+                    <li id="copilotStatus">Co-pilot ${copilotName} Ready</li>
                     <li id="fuelStatus">Fuel level high enough for launch</li>
                     <li id="cargoStatus">Cargo mass low enough for launch</li>
                 </ol>
@@ -45,28 +73,25 @@ window.addEventListener("load", function(){
       event.preventDefault();  
      
       
-      if (fuelLevel < 10000 || cargoMass > 10000){
-         faultyItems.style = "visibility: visible";
+      if (fuelLevel < 10000){
+         faultyItems.style.visibility = "visible";
          let fuelStatus = document.querySelector("#fuelStatus"); 
          fuelStatus.innerText = 'There\'s not enough fuel for the journey';
          launchStatus.innerText = "Shuttle not ready for launch";   
-         let cargoStatus = document.querySelector('#cargoStatus'); 
-         cargoStatus.innerText = 'There\'s too much in the cargo hold.'
          launchStatus.style = "color: red";
-         event.preventDefault(); 
+      } 
+      
+
+      if(cargoMass > 10000){
+         faultyItems.style.visibility = "visible"; 
+         let cargoStatus = document.querySelector('#cargoStatus'); 
+         cargoStatus.innerText = `There's too much in the cargo hold.`
+         launchStatus.innerText = "Shuttle not ready for launch";   
+         launchStatus.style = "color: red";
       } else {
          launchStatus.style = "color: green"; 
          launchStatus.innerText = "Shuttle is ready for launch"; 
-      }
-
-      // if(cargoMass > 10000){
-      //    faultyItems.style.visibility = "visible"; 
-      //    let cargoStatus = document.querySelector('#cargoStatus'); 
-      //    cargoStatus.innerHtml = `There's too much in the cargo hold.`
-      //    launchStatus.innerText = "Shuttle not ready for launch";   
-      //    launchStatus.style = "color: red";
-      //    event.preventDefault(); 
-      // }; 
+      };
 
    });  
 }); 

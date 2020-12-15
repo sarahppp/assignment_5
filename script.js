3e99f49ec27eefead13c4b3ca8edec8715a75dcf
Sarah Poulton <poultonse@gmail.com>
2020-12-14 18:12:27 -0600

    streamlined conditionals and added non-number validation

--- script.js
+++ script.js
@@ -11,7 +11,12 @@
 </ol>
 <img src="${}">
 */
-
+    // if (pilotName.includes(nonletters)){
+      //    alert("That is not an appropriate name for a pilot.")
+      //    event.preventDefault(); 
+      // }; '`', '~', '!', '@', "#", '$', '%', '^', '7', '*', "(", ')', '-', "_", '+', '=']; 
+      // let integers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+   
 async function fetchTarget () {
    try{   
       let response = await fetch('https://handlers.education.launchcode.org/static/planets.json'); 
@@ -34,6 +39,13 @@ async function fetchTarget () {
    }
 };
 
+// function onlyAlphabet(input) {
+//    let letters = /^[a-zA-Z]+$/; 
+//    if(input.match(letters)){
+//       return true
+//    }; 
+// }; 
+
 window.addEventListener("load", function(){
    fetchTarget(); 
 
@@ -49,49 +61,53 @@ window.addEventListener("load", function(){
       let launchStatus = document.getElementById("launchStatus"); 
       fuelLevel = Number(fuelLevel); 
       cargoMass = Number(cargoMass); 
-      let nonletters = ['`', '~', '!', '@', "#", '$', '%', '^', '7', '*', "(", ')', '-', "_", '+', '=']; 
-
-      if (pilotName.value === "" || copilotName === "" || fuelLevel === "" || cargoMass === ""){
-         alert("All fields are required"); 
-         event.preventDefault(); 
-    };
-      // if (pilotName.includes(nonletters)){
-      //    alert("That is not an appropriate name for a pilot.")
-      //    event.preventDefault(); 
-      // }; 
+      let pilotNames = [pilotName, copilotName]; 
+      let letters = /^[a-zA-Z]+$/;
+      let numbers = /^[0-9]+$/; 
+      
       let updatedHtml = `
-                <ol>
-                    <li id="pilotStatus">Pilot ${pilotName} Ready</li>
-                    <li id="copilotStatus">Co-pilot ${copilotName} Ready</li>
-                    <li id="fuelStatus">Fuel level high enough for launch</li>
-                    <li id="cargoStatus">Cargo mass low enough for launch</li>
-                </ol>
-            </div>
-      `  
+         <ol>
+             <li id="pilotStatus">Pilot ${pilotName} Ready</li>
+             <li id="copilotStatus">Co-pilot ${copilotName} Ready</li>
+             <li id="fuelStatus">Fuel level high enough for launch</li>
+             <li id="cargoStatus">Cargo mass low enough for launch</li>
+         </ol>
+         `  
       let faultyItems = document.getElementById("faultyItems"); 
       faultyItems.innerHTML = updatedHtml;
-      event.preventDefault();  
      
-      
-      if (fuelLevel < 10000){
+      if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === ""){
+         alert("All fields are required"); 
+         launchStatus.innerText = "Awaiting Information Before Launch"; 
+         faultyItems.style.visibility = "hidden"; 
+         event.preventDefault(); 
+       } else if (pilotName.match(numbers)){        
+          alert("That name is not recognized");
+          launchStatus.innerText = "Awaiting Information Before Launch"; 
+          event.preventDefault(); 
+       } else if (copilotName.match(numbers)){        
+         alert("That name is not recognized");
+         launchStatus.innerText = "Awaiting Information Before Launch"; 
+         event.preventDefault(); 
+      }  else if (fuelLevel < 10000){
          faultyItems.style.visibility = "visible";
          let fuelStatus = document.querySelector("#fuelStatus"); 
          fuelStatus.innerText = 'There\'s not enough fuel for the journey';
          launchStatus.innerText = "Shuttle not ready for launch";   
          launchStatus.style = "color: red";
-      } 
-      
-
-      if(cargoMass > 10000){
+         event.preventDefault(); 
+      } else if(cargoMass > 10000){
          faultyItems.style.visibility = "visible"; 
          let cargoStatus = document.querySelector('#cargoStatus'); 
          cargoStatus.innerText = `There's too much in the cargo hold.`
          launchStatus.innerText = "Shuttle not ready for launch";   
          launchStatus.style = "color: red";
+         event.preventDefault(); 
       } else {
          launchStatus.style = "color: green"; 
          launchStatus.innerText = "Shuttle is ready for launch"; 
-      };
-
+         faultyItems.style.visibility = "visible"; 
+         event.preventDefault();  
+      }; 
    });  
 }); 
